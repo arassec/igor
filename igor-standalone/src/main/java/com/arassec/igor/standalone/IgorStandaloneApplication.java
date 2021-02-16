efb6eb2e438ad68d043b1fa6848e505548545515
Andreas Sensen <andreas.sensen@arassec.com>
2021-02-16 16:27:21 +0100

    chore: Updated frontend-maven-plugin to version 1.11.2 and added Spring-Boot banner.

--- igor-standalone/src/main/java/com/arassec/igor/standalone/IgorStandaloneApplication.java
+++ igor-standalone/src/main/java/com/arassec/igor/standalone/IgorStandaloneApplication.java
@@ -3,7 +3,6 @@ package com.arassec.igor.standalone;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -27,19 +26,14 @@ public class IgorStandaloneApplication {
     /**
      * Creates Open-API information.
      *
-     * @param appDescription Description of the application from the module's pom.xml file.
-     * @param appVersion     Version of the application from the module's pom.xml file.
-     *
      * @return An {@link OpenAPI} specification.
      */
     @Bean
-    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application-version}") String appVersion) {
+    public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Igor Application API")
-                        .version(appVersion)
-                        .description(appDescription)
-                        .license(new License().name("MIT").url("https://github.com/arassec/igor/blob/master/LICENSE")));
+            .info(new Info()
+                .title("Igor Application API")
+                .license(new License().name("MIT").url("https://github.com/arassec/igor/blob/master/LICENSE")));
     }
 
 }
